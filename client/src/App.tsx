2c1b52ed62d8b47ce3b8ddce0abfbe44d41901ab
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-20 10:24:15 -0700

    Updated viteconfig.ts pathing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { useBoard } from "@hooks/useBoard";
 import GameBoard from "@components/molecules/GameBoard";
 import { BOARD_SIZE } from "@data/constants";
-import { IBoard } from "@models/types";
 const { ROWS, COLUMNS } = BOARD_SIZE;
 
 function App(): JSX.Element {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,5 +4,7 @@ import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteAliases()],
+  plugins: [react(), ViteAliases({
+    dir: "./src"
+  })],
 });
