a92693175a498eb2e34467354d179ac3e78963d5
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-31 08:14:08 -0700

    config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,6 @@ import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/takuyadev/battleship-clone/',
   plugins: [
     react(),
     ViteAliases({
