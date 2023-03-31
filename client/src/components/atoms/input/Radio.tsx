043952a3fab571f1497458ff1b7bae374c3eb4e3
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-03-31 13:30:49 -0700

    Fix radio dark mode bug

--- client/src/components/atoms/input/Radio.tsx
+++ client/src/components/atoms/input/Radio.tsx
@@ -21,7 +21,7 @@ const Radio = ({
       <input id={id} type='radio' className='hidden peer' {...props} />
       <label
         htmlFor={id}
-        className={`${className} font-body gap-8 duration-200 w-full inline-flex items-center justify-between p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-indigo-500 peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700`}
+        className={`${className} font-body gap-8 duration-200 w-full inline-flex items-center justify-between p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-indigo-600 peer-checked:text-indigo-600 hover:text-gray-600 hover:bg-gray-100`}
       >
         <div className='block'>
           <div className='w-full text-lg font-semibold'>{title}</div>
